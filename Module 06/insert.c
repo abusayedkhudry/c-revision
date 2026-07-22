233f08bf39b32e96f9e46436902612e2dce31f9e
Abu Sayed Khudry <khudryabusayed@gmail.com>
2026-07-22 22:49:44 +0600

    Add Array self practice

---
+++ Module 06/insert.c
@@ -0,0 +1,26 @@
+//self practice
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int ar[n + 1];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &ar[i]);
+    }
+    int indx, val;
+    scanf("%d %d", &indx, &val);
+
+    for (int i = n; i > indx; i--)
+    {
+        ar[i] = ar[i - 1];
+    }
+    ar[indx] = val;
+    for (int i = 0; i < n + 1; i++)
+    {
+        printf("%d ", ar[i]);
+    }
+
+    return 0;
+}
