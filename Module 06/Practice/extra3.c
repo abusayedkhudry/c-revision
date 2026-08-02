9c96ed281d29c2b895c7015d46ea048e8f2d5252
Abu Sayed Khudry <khudryabusayed@gmail.com>
2026-08-02 14:29:24 +0600

    added extra problem in module 06 practice

---
+++ Module 06/Practice/extra3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int ar[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d ", &ar[i]);
+    }
+    for (int i = 0, j = n - 1; j > i; i++, j--)
+    {
+        int temp = ar[i];
+        ar[i] = ar[j];
+        ar[j] = temp;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", ar[i]);
+    }
+
+    return 0;
+}
